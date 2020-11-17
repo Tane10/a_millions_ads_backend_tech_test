61da53232db53f62cf1762dd5c650e64ff911558
JoshThom-itv <joshua.thomas.houlker@itv.com>
2020-11-17 21:46:49 +0000

    added some error handling

--- src/routes/postcode.route.ts
+++ src/routes/postcode.route.ts
@@ -5,19 +5,41 @@ const router: Router = express.Router();
 const postcodeClass = new PostCodesClass();
 
 router.post("/api/v1/postcode/latlong", async (req: Request, res: Response) => {
-    const postcodes = req.body;
+    let postcodes = req.body;
 
-    const postcodesLatLong = await postcodeClass.getLatlongFromPostcode(postcodes);
+    if (postcodes.postcodes === undefined) res.status(400).send("request body is empty");
+    if (postcodes.postcodes.length === 0) res.status(400).send("Please add some postcodes to the request body");
+    else {
 
-    res.status(200).send(postcodesLatLong);
+        postcodes.postcodes.map((val: string, idx: number) => {
+            if (val === "" || val === " ") {
+                postcodes.postcodes.splice(idx, 1)
+            }
+        });
+
+        const postcodesLatLong = await postcodeClass.getLatlongFromPostcode(postcodes);
+
+        res.status(200).send(postcodesLatLong);
+    }
 })
 
 router.post("/api/v1/postcode/details", async (req: Request, res: Response) => {
-    const postcodes = req.body;
+    let postcodes = req.body;
 
-    const postcodesDeatils = await postcodeClass.getDetailsFromPostcode(postcodes);
+    if (postcodes.postcodes === undefined) res.status(400).send("request body is empty");
+    if (postcodes.postcodes.length === 0) res.status(400).send("Please add some postcodes to the request body");
+    else {
 
-    res.status(200).send(postcodesDeatils);
+        postcodes.postcodes.map((val: string, idx: number) => {
+            if (val === "" || val === " ") {
+                postcodes.postcodes.splice(idx, 1)
+            }
+        });
+
+        const postcodesDeatils = await postcodeClass.getDetailsFromPostcode(postcodes);
+
+        res.status(200).send(postcodesDeatils);
+    }
 })
 
 export default router;
